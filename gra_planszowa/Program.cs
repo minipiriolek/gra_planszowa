395e158ad651339ead299786ecf7fdb105570a26
olke <jajo@bajo.pl>
2024-12-10 15:02:24 +0100

    klasa player

--- gra_planszowa/Program.cs
+++ gra_planszowa/Program.cs
@@ -1,9 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+public class Player
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public string Score { get; set; }
 
+    public void Ruch_na_planszy(int steps)
+    {
+        Position += steps;
+    }
+
+    public void Aktualizacja_punktow(int punkty)
+    {
+        Score += punkty;
+    }
+}
 internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
     }
 }
